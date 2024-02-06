cc98f56246503ccd4fa926af2882503cde843cf8
Stefan Bernhardsgrütter <stefan.bernhardsgruetter@redguard.ch>
2024-02-06 15:08:00 +0100

    MASV Menu added

--- App.tsx
+++ App.tsx
@@ -7,25 +7,21 @@
 
 import * as React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {View, Text, Button} from 'react-native';
+import {View, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-// function HomeScreen({navigation}) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Go to Details"
-//         onPress={() => navigation.navigate('Details')}
-//       />
-//     </View>
-//   );
-// }
+const STORAGE_COLOR = '#df5c8c';
+const CRYPTO_COLOR = '#f65928';
+const AUTH_COLOR = '#f09236';
+const NETWORK_COLOR = '#f2c200';
+const PLATFORM_COLOR = '#4fb990';
+const CODE_COLOR = '#5facd3';
+const RESILIENCE_COLOR = '#317bc0';
+const PRIVACY_COLOR = '#8b5f9e';
 
 function HomeScreen({navigation}): React.JSX.Element {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Button
         onPress={() => navigation.navigate('Notification')}
         title="Go to notifications"
@@ -34,182 +30,105 @@ function HomeScreen({navigation}): React.JSX.Element {
   );
 }
 
-// function DetailsScreen({navigation}) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Details Screen</Text>
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() => navigation.push('Details')}
-//       />
-//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-//       <Button title="Go back" onPress={() => navigation.goBack()} />
-//       <Button
-//         title="Go back to first screen in stack"
-//         onPress={() => navigation.popToTop()}
-//       />
-//     </View>
-//   );
-// }
-
 function NotificationsScreen({navigation}): React.JSX.Element {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
   );
 }
-
-// const Stack = createNativeStackNavigator();
 
 const Drawer = createDrawerNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator> */}
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Automation" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-STORAGE" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-CRYPTO" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-AUTH" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-NETWORK" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-PLATFORM" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-CODE" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-RESILIENCE" component={NotificationsScreen} />
-        <Drawer.Screen name="MASVS-PRIVACY" component={NotificationsScreen} />
-
+        <Drawer.Screen
+          name="MASVS-STORAGE"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: STORAGE_COLOR},
+            drawerItemStyle: {backgroundColor: STORAGE_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-CRYPYO"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: CRYPTO_COLOR},
+            drawerItemStyle: {backgroundColor: CRYPTO_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-AUTH"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: AUTH_COLOR},
+            drawerItemStyle: {backgroundColor: AUTH_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-NETWORK"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: NETWORK_COLOR},
+            drawerItemStyle: {backgroundColor: NETWORK_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-PLATFORM"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: PLATFORM_COLOR},
+            drawerItemStyle: {backgroundColor: PLATFORM_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-CODE"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: CODE_COLOR},
+            drawerItemStyle: {backgroundColor: CODE_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-RESILIENCE"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: RESILIENCE_COLOR},
+            drawerItemStyle: {backgroundColor: RESILIENCE_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
+        <Drawer.Screen
+          name="MASVS-PRIVACY"
+          component={NotificationsScreen}
+          options={{
+            headerStyle: {backgroundColor: PRIVACY_COLOR},
+            drawerItemStyle: {backgroundColor: PRIVACY_COLOR},
+            drawerLabelStyle: {color: 'white'},
+            headerTitleStyle: {color: 'white'},
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
-// import 'react-native-gesture-handler';
-// import React from 'react';
-// import type {PropsWithChildren} from 'react';
-// import {
-//   Alert,
-//   Button,
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function MyButton(): React.JSX.Element {
-//   return (
-//     <Button
-//       title="Press me"
-//       onPress={() => Alert.alert('Simple Button pressed')}
-//     />
-//   );
-// }
-
-// function Section({children, title}: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             <MyButton />
-//             Edit <Text style={styles.highlight}>Edfasdf</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
