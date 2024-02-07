1345488f7fad8b59754e45fc9ce35f6fc019131c
Stefan Bernhardsgrütter <s.bernhardsgruetter@gmail.com>
2024-02-07 10:49:50 +0100

    typo

--- src/screens/categoryScreen/categoryScreen.tsx
+++ src/screens/categoryScreen/categoryScreen.tsx
@@ -1,10 +1,32 @@
-import {ScrollView, Text} from 'react-native';
+import {ScrollView, Text, TouchableOpacity} from 'react-native';
 import styles from './style.tsx';
+import React from 'react';
 
 function CategoryScreen({route}: {route: any}): React.JSX.Element {
   return (
     <ScrollView style={styles.categoryDescription}>
       <Text>{route.params.description}</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
