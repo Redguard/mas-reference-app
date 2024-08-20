39e302d79d507bbca8932dbd2e7cd7fdc10308cf
Stefan Bernhardsgrütter <stefan.bernhardsgruetter@redguard.ch>
2024-08-20 16:26:13 +0200

    output refactoring

--- android/app/src/main/java/com/masreferenceapp/auth/AuthKeyAccess.java
+++ android/app/src/main/java/com/masreferenceapp/auth/AuthKeyAccess.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.masreferenceapp.ReturnStatus;
 import com.masreferenceapp.Status;
 
 import javax.crypto.Cipher;
@@ -29,15 +30,47 @@ public class AuthKeyAccess extends ReactContextBaseJavaModule {
         return "AuthKeyAccess";
     }
 
-    @ReactMethod(isBlockingSynchronousMethod = true)
-    public String accessKeyWithoutPrompt(){
 
+//    @ReactMethod(isBlockingSynchronousMethod = true)
+//    public String accessKeyWithoutPrompt(){
+//
+//        try {
+//            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+//            KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
+//                    "masAccessTests",
+//                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT).
+//                    setUserAuthenticationRequired(true).
+//                    setBlockModes(KeyProperties.BLOCK_MODE_CBC).
+//                    setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7).
+//                    setKeySize(256).
+//                    build();
+//
+//            keygen.init(spec);
+//            SecretKey key = keygen.generateKey();
+//
+//            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7PADDING");
+//            cipher.init(Cipher.ENCRYPT_MODE, key);
+//            byte[] encrypted = cipher.doFinal("someSecret Plaintext".getBytes());
+//
+//
+//            ReturnStatus r = new ReturnStatus("OK", "Accessed key: " +  key.toString());
+//            return r.toJsonString();
+//
+//        } catch (Exception e) {
+//            ReturnStatus r = new ReturnStatus("FAIL", "Exception: " + e.toString());
+//            return r.toJsonString();        }
+//
+//    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String deviceCredentialsRequired(){
         try {
             KeyGenerator keygen = KeyGenerator.getInstance("AES");
             KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
                     "masAccessTests",
                     KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT).
                     setUserAuthenticationRequired(true).
+                    setUserAuthenticationParameters(60, KeyProperties.AUTH_DEVICE_CREDENTIAL).
                     setBlockModes(KeyProperties.BLOCK_MODE_CBC).
                     setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7).
                     setKeySize(256).
@@ -46,16 +79,39 @@ public class AuthKeyAccess extends ReactContextBaseJavaModule {
             keygen.init(spec);
             SecretKey key = keygen.generateKey();
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7PADDING");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] encrypted = cipher.doFinal("someSecret Plaintext".getBytes());
-
-            return Status.status("OK", key.toString());
+            ReturnStatus r = new ReturnStatus("OK", "Accessed a key which required a device auth (Pin/Password/Pattern).");
+            return r.toJsonString();
 
         } catch (Exception e) {
-            return Status.status("FAIL", e.toString());
+            ReturnStatus r = new ReturnStatus("FAIL", "Exception: " + e.toString());
+            return r.toJsonString();
         }
+    }
 
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String biometryRequired(){
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
+                    "masAccessTests",
+                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT).
+                    setUserAuthenticationRequired(true).
+                    setUserAuthenticationParameters(60, KeyProperties.AUTH_BIOMETRIC_STRONG).
+                    setBlockModes(KeyProperties.BLOCK_MODE_CBC).
+                    setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7).
+                    setKeySize(256).
+                    build();
+
+            keygen.init(spec);
+            SecretKey key = keygen.generateKey();
+
+            ReturnStatus r = new ReturnStatus("OK", "Accessed a key which required strong biometric auth.");
+            return r.toJsonString();
+
+        } catch (Exception e) {
+            ReturnStatus r = new ReturnStatus("FAIL", "Exception: " + e.toString());
+            return r.toJsonString();
+        }
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
@@ -66,7 +122,7 @@ public class AuthKeyAccess extends ReactContextBaseJavaModule {
                     "masAccessTests",
                     KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT).
                     setUserAuthenticationRequired(true).
-                    setUserAuthenticationParameters(2000000, KeyProperties.AUTH_BIOMETRIC_STRONG).
+                    setUserAuthenticationParameters(31536000, KeyProperties.AUTH_DEVICE_CREDENTIAL).
                     setBlockModes(KeyProperties.BLOCK_MODE_CBC).
                     setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7).
                     setKeySize(256).
@@ -75,10 +131,12 @@ public class AuthKeyAccess extends ReactContextBaseJavaModule {
             keygen.init(spec);
             SecretKey key = keygen.generateKey();
 
-            return Status.status("OK", key.toString());
+            ReturnStatus r = new ReturnStatus("OK", "Accessed a key with validity timeout of a year.");
+            return r.toJsonString();
 
         } catch (Exception e) {
-            return Status.status("FAIL", e.toString());
+            ReturnStatus r = new ReturnStatus("FAIL", "Exception: " + e.toString());
+            return r.toJsonString();
         }
     }
 }
