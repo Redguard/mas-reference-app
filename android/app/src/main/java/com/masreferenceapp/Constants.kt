8c6ff9ee4754f4e94ba7c671d9e70ca8b1b42fb8
Stefan Bernhardsgrütter <s.bernhardsgruetter@gmail.com>
2024-09-19 23:41:48 +0200

    refactoring of name

--- android/app/src/main/java/com/masreferenceapp/Constants.kt
+++ android/app/src/main/java/com/masreferenceapp/Constants.kt
@@ -3,7 +3,7 @@ package com.masreferenceapp
 object Constants {
     const val localWebViewDomain = "file:///android_asset/masTestPage.html"
     const val localWebViewJavaScriptBridge = "file:///android_asset/masJavaScriptBridge.html"
-    const val remoteWebViewHttpDomain = "redguard.ch"
-    const val remoteWebViewHttpPort = 9800
-    const val remoteWebViewHttpsDomain = "redguard.ch"
+    const val remoteHttpDomain = "redguard.ch"
+    const val remoteHttpPort = 9800
+    const val remoteHttpsDomain = "redguard.ch"
 }
