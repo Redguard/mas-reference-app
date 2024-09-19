2550bc46712e80d93ccb46e750983b55bb2ab136
Stefan Bernhardsgrütter <stefan.bernhardsgruetter@redguard.ch>
2024-09-19 11:16:12 +0200

    new android stub code added

--- android/app/src/main/java/com/masreferenceapp/network/NetworkLocalNetwork.kt
+++ android/app/src/main/java/com/masreferenceapp/network/NetworkLocalNetwork.kt
@@ -3,6 +3,7 @@ package com.masreferenceapp.network
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.masreferenceapp.ReturnStatus
 import com.masreferenceapp.Status
 
 class NetworkLocalNetwork(var context: ReactApplicationContext) : ReactContextBaseJavaModule(
@@ -16,4 +17,35 @@ class NetworkLocalNetwork(var context: ReactApplicationContext) : ReactContextBa
     fun putString(): String {
         return Status.status("OK", "Message")
     }
+
+        
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun access(): String {
+        val r = ReturnStatus("OK", "Android code stub.")
+        return r.toJsonString()
+    }
+
+        
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun hostHttpServer(): String {
+        val r = ReturnStatus("OK", "Android code stub.")
+        return r.toJsonString()
+    }
+
+        
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun hostTcpServer(): String {
+        val r = ReturnStatus("OK", "Android code stub.")
+        return r.toJsonString()
+    }
+
+        
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun hostUDPServer(): String {
+        val r = ReturnStatus("OK", "Android code stub.")
+        return r.toJsonString()
+    }
+
+    //@method
+
 }
