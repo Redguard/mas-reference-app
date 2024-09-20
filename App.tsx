08c876a5225b26be5a662a597b481b1b6fff97c8
Stefan Bernhardsgrütter <stefan.bernhardsgruetter@redguard.ch>
2024-09-20 15:21:33 +0200

    made settings persistant

--- App.tsx
+++ App.tsx
@@ -14,12 +14,51 @@ import HomeScreen from './src/screens/homeScreen/homeScreen.tsx';
 import SettingsScreen from './src/screens/settingsScreen/settingsScreen.tsx';
 import appContent from './src/appContent.tsx';
 import HeaderBackground from './src/screens/header/headerBackground.tsx';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import { Settings } from 'react-native-feather';
+
+export type MasSettings = {
+  httpTestDomain: string;
+  httpsTestDomain: string;
+  canaryToken: string;
+  androidApiKey: string;
+};
+
 
 const Drawer = createDrawerNavigator();
 
+async function initGlobalSettings(){
+  try {
+    const settings = await EncryptedStorage.getItem('MasReferenceAppSettings');
+    if (settings === null) {
 
+      console.log('First start, storing default settings.');
+
+      const masSettings: MasSettings = {
+        httpTestDomain: 'redguard.ch',
+        httpsTestDomain: 'redguard.ch',
+        canaryToken: '__MAS.SECRET.eL4$wV__',
+        androidApiKey: '',
+      };
+      try {
+        await EncryptedStorage.setItem(
+            'MasReferenceAppSettings',
+            JSON.stringify(masSettings)
+        );
+        } catch (error) {
+          console.log(error);
+    }
+    }
+
+    } catch (error) {
+      console.log(error);
+    }
+}
 
 function App(): React.JSX.Element {
+
+  initGlobalSettings();
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
