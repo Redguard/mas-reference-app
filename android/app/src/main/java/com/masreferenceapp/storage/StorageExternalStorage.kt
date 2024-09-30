c4ed270cfc7bf75cb66dd0b31204fd61e57fe898
Stefan Bernhardsgrütter <stefan.bernhardsgruetter@redguard.ch>
2024-09-30 13:59:27 +0200

    android refactoring

--- android/app/src/main/java/com/masreferenceapp/storage/StorageExternalStorage.kt
+++ android/app/src/main/java/com/masreferenceapp/storage/StorageExternalStorage.kt
@@ -24,54 +24,54 @@ class StorageExternalStorage(private val context: ReactApplicationContext) : Rea
     }
     
 
-    @ReactMethod(isBlockingSynchronousMethod = true)
-    fun writeFileOutput(): String {
-        val fileContents = "Some secret Message in the external sandbox!"
-        val r = ReturnStatus()
+//    @ReactMethod(isBlockingSynchronousMethod = true)
+//    fun writeFileOutput(): String {
+//        val fileContents = "Some secret Message in the external sandbox!"
+//        val r = ReturnStatus()
+//
+//        try {
+//            val file = "masExternalTestfile"
+//            val appSpecificExternalFile = File(context.getExternalFilesDir(null), file)
+//            FileOutputStream(appSpecificExternalFile).use { fos ->
+//                fos.write(fileContents.toByteArray(StandardCharsets.UTF_8))
+//            }
+//            r.addStatus("OK", "File successfully written.")
+//        } catch (e: Exception) {
+//            r.addStatus("FAIL", e.toString())
+//
+//        }
+//        return r.toJsonString()
+//    }
 
-        try {
-            val file = "masExternalTestfile"
-            val appSpecificExternalFile = File(context.getExternalFilesDir(null), file)
-            FileOutputStream(appSpecificExternalFile).use { fos ->
-                fos.write(fileContents.toByteArray(StandardCharsets.UTF_8))
-            }
-            r.addStatus("OK", "File successfully written.")
-        } catch (e: Exception) {
-            r.addStatus("FAIL", e.toString())
-
-        }
-        return r.toJsonString()
-    }
-
-    @ReactMethod(isBlockingSynchronousMethod = true)
-    fun readTextFile(): String {
-        val r = ReturnStatus()
-
-
-
-        // Ensure the file exists by writing it first
-        writeFileOutput()
-
-        try {
-            val file = "masExternalTestfile"
-            val appSpecificExternalFile = File(context.getExternalFilesDir(null), file)
-            FileInputStream(appSpecificExternalFile).use { fis ->
-                InputStreamReader(fis, StandardCharsets.UTF_8).use { inputStreamReader ->
-                    BufferedReader(inputStreamReader).use { reader ->
-                        val text = StringBuilder()
-                        text.append(reader.readText())
-                        r.addStatus("OK", "File successfully read. Content: $text")
-
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            r.addStatus("FAIL", e.toString())
-
-        }
-
-        return r.toJsonString()
-    }
+//    @ReactMethod(isBlockingSynchronousMethod = true)
+//    fun readTextFile(): String {
+//        val r = ReturnStatus()
+//
+//
+//
+//        // Ensure the file exists by writing it first
+//        writeFileOutput()
+//
+//        try {
+//            val file = "masExternalTestfile"
+//            val appSpecificExternalFile = File(context.getExternalFilesDir(null), file)
+//            FileInputStream(appSpecificExternalFile).use { fis ->
+//                InputStreamReader(fis, StandardCharsets.UTF_8).use { inputStreamReader ->
+//                    BufferedReader(inputStreamReader).use { reader ->
+//                        val text = StringBuilder()
+//                        text.append(reader.readText())
+//                        r.addStatus("OK", "File successfully read. Content: $text")
+//
+//                    }
+//                }
+//            }
+//        } catch (e: Exception) {
+//            r.addStatus("FAIL", e.toString())
+//
+//        }
+//
+//        return r.toJsonString()
+//    }
 
 
         
@@ -109,10 +109,10 @@ class StorageExternalStorage(private val context: ReactApplicationContext) : Rea
             val externalFile = context.getExternalFilesDir(type)
             if (externalFile == null) {
                 status.append("[FAIL]")
-                r.addStatus("FAIL", "No external file.")
+                r.addStatus("FAIL", "No external Dir.")
             } else {
                 status.append("[OK]")
-                r.addStatus("OK", "External file: $externalFile" )
+                r.addStatus("OK", "External dir is: $externalFile" )
 
             }
         }
--- android/app/src/main/java/com/masreferenceapp/storage/StorageJavaFileIo.kt
+++ android/app/src/main/java/com/masreferenceapp/storage/StorageJavaFileIo.kt
@@ -1,10 +1,16 @@
 package com.masreferenceapp.storage
 
+import android.content.Context
+import android.os.Environment
+import androidx.core.content.ContextCompat
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.masreferenceapp.MasSettings
 import com.masreferenceapp.ReturnStatus
-
+import com.masreferenceapp.SensitiveData
+import java.io.File
+import java.nio.charset.StandardCharsets
 
 
 class StorageJavaFileIo(var context: ReactApplicationContext) : ReactContextBaseJavaModule(
@@ -16,31 +22,93 @@ class StorageJavaFileIo(var context: ReactApplicationContext) : ReactContextBase
 
         
     @ReactMethod(isBlockingSynchronousMethod = true)
-    fun writeFileSandbox(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
-        return r.toJsonString()
-    }
-
-        
-    @ReactMethod(isBlockingSynchronousMethod = true)
     fun writeExternal(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
+        val r = ReturnStatus()
+        writeFileOutputMODE(Context.MODE_PRIVATE, r, MasSettings.getCanaryToken())
         return r.toJsonString()
     }
 
-        
+
     @ReactMethod(isBlockingSynchronousMethod = true)
     fun writeExternalWorldWritable(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
+        val r = ReturnStatus()
+        writeExternalFileOutputMODE(Context.MODE_WORLD_WRITEABLE, r, SensitiveData.data)
+        return r.toJsonString()
+    }
+
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun writeExternalWorldReadable(): String {
+        val r = ReturnStatus()
+        writeExternalFileOutputMODE(Context.MODE_WORLD_READABLE, r, SensitiveData.data)
+        return r.toJsonString()
+    }
+
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun writeSandboxWorldReadable(): String {
+        val r = ReturnStatus()
+        writeFileOutputMODE(Context.MODE_WORLD_READABLE, r, SensitiveData.data)
+        return r.toJsonString()
+    }
+
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun writeSandboxWorldWritable(): String {
+        val r = ReturnStatus()
+        writeFileOutputMODE(Context.MODE_WORLD_WRITEABLE, r, SensitiveData.data)
+        return r.toJsonString()
+    }
+
+
+        
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun writeCtFileSandbox(): String {
+        val r = ReturnStatus()
+        writeExternalFileOutputMODE(Context.MODE_PRIVATE, r, MasSettings.getCanaryToken())
         return r.toJsonString()
     }
 
         
     @ReactMethod(isBlockingSynchronousMethod = true)
-    fun writeExternalWorldReadable(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
+    fun writeSensitiveFileSandbox(): String {
+        val r = ReturnStatus()
+        writeFileOutputMODE(Context.MODE_PRIVATE, r, SensitiveData.data)
         return r.toJsonString()
     }
+
+    private fun writeFileOutputMODE(mode: Int, r: ReturnStatus, content: String) {
+        val random = (0..1000).random()
+        val filename = "Internal_MAS_Test_FILE_$random"
+
+        try {
+            val file = File(context.filesDir, filename)
+            context.openFileOutput(filename, mode).use { fos ->
+                fos.write(content.toByteArray(StandardCharsets.UTF_8))
+                r.addStatus("OK", "File successfully written: ${file.absolutePath}")
+            }
+        } catch (e: Exception) {
+            r.addStatus("FAIL", e.toString())
+        }
+    }
+
+    private fun writeExternalFileOutputMODE(mode: Int, r: ReturnStatus, content: String) {
+        val random = (0..1000).random()
+        val filename = "External_MAS_Test_FILE_$random"
+
+        try {
+            val externalStorageDir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+            val file = File(externalStorageDir, filename)
+
+            context.openFileOutput(filename, mode).use { fos ->
+                fos.write(content.toByteArray(StandardCharsets.UTF_8))
+                r.addStatus("OK", "File successfully written: ${file.absoluteFile}")
+            }
+        } catch (e: Exception) {
+            r.addStatus("FAIL", e.toString())
+        }
+    }
+
 
     //@method
 }
