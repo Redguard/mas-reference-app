7d5b3fb6249fc81f2761b387e8435086b1611ca3
Stefan Bernhardsgrütter <s.bernhardsgruetter@gmail.com>
2025-01-09 16:42:19 +0100

    added server components

--- android/app/src/main/java/com/masreferenceapp/auth/AuthHttpBasicAuth.kt
+++ android/app/src/main/java/com/masreferenceapp/auth/AuthHttpBasicAuth.kt
@@ -1,13 +1,18 @@
 package com.masreferenceapp.auth
 
+import android.webkit.WebView
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.masreferenceapp.MasSettings
 import com.masreferenceapp.ReturnStatus
+import com.masreferenceapp.auth.helpers.HttpBasicAuthWebViewClient
+import java.net.Authenticator
+import java.net.HttpURLConnection
+import java.net.PasswordAuthentication
+import java.net.URL
 
-
-
-class AuthHttpBasicAuth(var context: ReactApplicationContext) : ReactContextBaseJavaModule(
+open class AuthHttpBasicAuth(var context: ReactApplicationContext) : ReactContextBaseJavaModule(
     context
 ) {
     override fun getName(): String {
@@ -17,15 +22,38 @@ class AuthHttpBasicAuth(var context: ReactApplicationContext) : ReactContextBase
         
     @ReactMethod(isBlockingSynchronousMethod = true)
     fun javaNet(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
-        return r.toJsonString()
+        try{
+            val url = URL("https://" + MasSettings.getData("testDomain") + "/basicAuth.html")
+
+            Authenticator.setDefault(object : Authenticator() {
+                override fun getPasswordAuthentication(): PasswordAuthentication {
+                    return PasswordAuthentication("mas", "Passw0rd".toCharArray())
+                }
+            })
+
+            val c = url.openConnection() as HttpURLConnection
+            c.connect()
+
+            val r = ReturnStatus("OK", "Successfully connected to the URL $url. Response code was: ${c.responseCode}")
+            return r.toJsonString()
+        }
+
+        catch (e: Exception) {
+            val r = ReturnStatus("FAIL", "Exception: $e")
+            return r.toJsonString()
+        }
     }
 
         
     @ReactMethod(isBlockingSynchronousMethod = true)
     fun webView(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
-        return r.toJsonString()
+
+        val testDomain = MasSettings.getData("testDomain")
+        val wv = WebView(context)
+        wv.webViewClient = HttpBasicAuthWebViewClient()
+        wv.loadUrl("https://$testDomain/basicAuth.html")
+        return ReturnStatus("OK", "Resource loaded. URL of the WebView: " + wv.url).toJsonString()
+
     }
 
     //@method
--- android/app/src/main/java/com/masreferenceapp/network/NetworkTlsConfig.kt
+++ android/app/src/main/java/com/masreferenceapp/network/NetworkTlsConfig.kt
@@ -5,6 +5,8 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.masreferenceapp.MasSettings
 import com.masreferenceapp.ReturnStatus
+import java.net.HttpURLConnection
+import java.net.URL
 import javax.net.ssl.SSLParameters
 import javax.net.ssl.SSLSocket
 import javax.net.ssl.SSLSocketFactory
@@ -105,7 +107,41 @@ class NetworkTlsConfig(var context: ReactApplicationContext) : ReactContextBaseJ
         
     @ReactMethod(isBlockingSynchronousMethod = true)
     fun acceptBadTLS(): String {
-        val r = ReturnStatus("OK", "Android code stub.")
+
+        val badDomains = arrayOf(
+            "https://expired.badssl.com/",
+            "https://wrong.host.badssl.com/",
+            "https://self-signed.badssl.com/",
+            "https://untrusted-root.badssl.com/",
+            "https://revoked.badssl.com/",
+            "https://pinning-test.badssl.com/",
+            "https://dh480.badssl.com/",
+            "https://dh512.badssl.com/",
+            "https://dh1024.badssl.com/",
+            "https://dh-small-subgroup.badssl.com/",
+            "https://dh-composite.badssl.com/",
+            "https://rc4-md5.badssl.com/",
+            "https://rc4.badssl.com/",
+            "https://3des.badssl.com/",
+            "https://null.badssl.com/",
+            "https://sha1-2017.badssl.com/",
+            "https://sha1-intermediate.badssl.com/",
+            "https://invalid-expected-sct.badssl.com/"
+            )
+
+        val r = ReturnStatus()
+
+        for (domain in badDomains){
+            try{
+                val url = URL(domain)
+                val connection = url.openConnection() as HttpURLConnection
+                val responseCode = connection.responseCode
+                r.addStatus("FAIL", "App established connection to $domain . Response Code is $responseCode")
+
+            } catch (e : Exception) {
+                r.addStatus("OK", "App did not established connection to $domain. Error: $e")
+            }
+        }
         return r.toJsonString()
     }
 
