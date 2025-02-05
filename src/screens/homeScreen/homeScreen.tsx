1407b59ca34a9020ffa4ae6e099e930bbc67b214
Stefan Bernhardsgrütter <s.bernhardsgruetter@gmail.com>
2025-02-05 10:10:47 +0100

    added stats

--- src/screens/homeScreen/homeScreen.tsx
+++ src/screens/homeScreen/homeScreen.tsx
@@ -2,13 +2,60 @@ import {Image, ScrollView, Text, View} from 'react-native';
 import React from 'react';
 import styles from './style.tsx';
 import testCases from '../../testcases.tsx';
+import appContent from '../../appContent.tsx';
+import {Table, Rows} from 'react-native-table-component';
 
 function Stats({}: any): React.JSX.Element {
   console.log(testCases);
 
+  const stats: {[id: string]: number} = {};
+
+  for (const c in appContent) {
+    const category = appContent[c].key;
+    // const categoryColor = appContent[c].color;
+    var i = 0;
+
+    for (const tc in testCases[category]) {
+      i += Object.keys(testCases[category][tc].testCases).length;
+    }
+    stats[category] = i;
+  }
+
   return (
-    <View style={styles.stats}>
-      <Text>Stats</Text>
+    <View style={styles.statsContainer}>
+      <View>
+        <Table>
+          <Rows
+            data={[['MAS Category', 'Number of Tests']]}
+            textStyle={[styles.statsText, styles.statsTextHeader]}
+          />
+          <Rows
+            data={[['Storage', stats.STORAGE]]}
+            textStyle={styles.statsText}
+          />
+          <Rows
+            data={[['Crypto', stats.CRYPTO]]}
+            textStyle={styles.statsText}
+          />
+          <Rows data={[['Auth', stats.AUTH]]} textStyle={styles.statsText} />
+          <Rows
+            data={[['Network', stats.NETWORK]]}
+            textStyle={styles.statsText}
+          />
+          <Rows
+            data={[['Platform', stats.PLATFORM]]}
+            textStyle={styles.statsText}
+          />
+          <Rows
+            data={[['Resilience', stats.RESILIENCE]]}
+            textStyle={styles.statsText}
+          />
+          <Rows
+            data={[['Privacy', stats.PRIVACY]]}
+            textStyle={styles.statsText}
+          />
+        </Table>
+      </View>
     </View>
   );
 }
@@ -29,6 +76,7 @@ function HomeScreen({}: any): React.JSX.Element {
             'The OWASP MAS Reference App implements different vulnerabilities and security-relevant features such as anti-tampering measures. \n\nThe app can be used to train testers in assessing the security of mobile apps. In addition it can be used to develop and test security scanners both static and dynamic ones.'
           }
         </Text>
+        <Text>Here are some statistics:</Text>
         <Stats />
       </View>
     </ScrollView>
